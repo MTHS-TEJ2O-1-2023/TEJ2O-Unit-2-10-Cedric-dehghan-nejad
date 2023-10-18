7d4f5b584f6ce59c928410075a0d4736e512faea
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-10-17 21:50:57 -0400

    typescript change 6

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
  * Created by: cedric
  * Created on: Oct 2023
  * This program tells you the light level 
-*/
+ */
 
 // variables
 let neopixelStrip: neopixel.Strip = null
@@ -19,48 +19,48 @@ neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
-input.onButtonPressed(Button.A, function() {
-    basic.clearScreen()
-    lightValue = input.lightLevel()
-    lightValue = Math.round(lightValue)
-    // turn neopixel 0 blue if light value is less than or equal to 0
-    if (lightValue <= 51) {
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
-        neopixelStrip.show()
-        basic.pause(1000)
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.show()
-    }
-        if (lightValue >= 52){
-        if (lightValue < 104) {
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
-        neopixelStrip.show()
-        basic.pause(1000)
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.show()
-    }}
-        if (lightValue >= 104){
-        if (lightValue < 156){
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Yellow))
-        neopixelStrip.show()
-        basic.pause(1000)
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.show() 
-        }}
+ input.onButtonPressed(Button.A, function() {
+  basic.clearScreen()
+  lightValue = input.lightLevel()
+  lightValue = Math.round(lightValue)
+  // turn neopixel 0 blue if light value is less than or equal to 0
+  if (lightValue <= 51) {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.show()
+    basic.pause(1000)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+  }
+  if (lightValue >= 52){
+  if (lightValue < 104) {
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
+    basic.pause(1000)
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+  }}
+  if (lightValue >= 104){
+  if (lightValue < 156){
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Yellow))
+    neopixelStrip.show()
+    basic.pause(1000)
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show() 
+  }}
 
-        if (lightValue >= 156){
-        if  (lightValue < 208){
-        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Yellow))
-        neopixelStrip.show()
-        basic.pause(1000)
-        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.show()
-    }}
-        if (lightValue >= 208){
-            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Purple))
-            neopixelStrip.show()
-            basic.pause(1000)
-            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-            neopixelStrip.show()
-    }
+  if (lightValue >= 156){
+  if  (lightValue < 208){
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Yellow))
+    neopixelStrip.show()
+    basic.pause(1000)
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+  }}
+  if (lightValue >= 208){
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Purple))
+    neopixelStrip.show()
+    basic.pause(1000)
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+  }
 })
